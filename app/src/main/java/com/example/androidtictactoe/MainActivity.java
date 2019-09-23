2eefc640b41e125163d7b9e2c43fc473154d4398
Nguyen Huu Tuan <tuan.nguyen1@pn.com.vn>
2019-09-23 10:29:29 +0700

    finished

---
+++ app/src/main/java/com/example/androidtictactoe/MainActivity.java
@@ -0,0 +1,108 @@
+package com.example.androidtictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button bt1, bt2, bt3, bt4, bt5, bt6, bt7, bt8, bt9;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bt1 = findViewById(R.id.button_1);
+        bt2 = findViewById(R.id.button_2);
+        bt3 = findViewById(R.id.button_3);
+        bt4 = findViewById(R.id.button_4);
+        bt5 = findViewById(R.id.button_5);
+        bt6 = findViewById(R.id.button_6);
+        bt7 = findViewById(R.id.button_7);
+        bt8 = findViewById(R.id.button_8);
+        bt9 = findViewById(R.id.button_9);
+
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonClick(view);
+            }
+        });
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonClick(view);
+            }
+        });
+        bt3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonClick(view);
+            }
+        });
+        bt4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonClick(view);
+            }
+        });
+        bt5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonClick(view);
+            }
+        });
+        bt6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonClick(view);
+            }
+        });
+        bt7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonClick(view);
+            }
+        });
+        bt8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonClick(view);
+            }
+        });
+        bt9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonClick(view);
+            }
+        });
+    }
+
+    int count = 0;
+
+    void buttonClick(View view) {
+        if (count % 2 == 0) {
+            ((Button) view).setText("X");
+        } else {
+            ((Button) view).setText("0");
+        }
+        count++;
+        checkWin();
+    }
+
+    void checkWin() {
+        kiemTraX(bt1, bt2, bt3);
+    }
+
+    void kiemTraX(Button btn1, Button btn2, Button btn3) {
+        if (btn1.getText().equals("X") && btn2.getText().equals("X") && btn3.getText().equals("X")) {
+            Toast.makeText(this, "X win", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
